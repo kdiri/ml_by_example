1b896c83c070a5f9d49227bf4b8155fcc4d4e392
kdiri <kemal.diri@sewan.fr>
2020-06-16 09:29:19 +0200

    Remove what already moved to src.

--- chapter3_spam_detection/constant/email_text.py
+++
@@ -1,4 +0,0 @@
-emails_test = [
-    "Subject: flat screens\nhello ,\nplease call or contact regarding the other flat screens\nrequested .\ntrisha tlapek - eb 3132 b\nmichael sergeev - eb 3132 a\nalso the sun blocker that was taken away from eb 3131 a .\ntrisha should two monitors also michael .\nthanks\nkevin moore",
-    "Subject: having problems in bed ? we can help !\ncialis allows men to enjoy a fully normal sex life without\nhaving to plan the sexual act .\nif we let things terrify us, life will not be worth living brevity is the soul of lingerie .\nsuspicion always haunts the guilty mind .",
-]
